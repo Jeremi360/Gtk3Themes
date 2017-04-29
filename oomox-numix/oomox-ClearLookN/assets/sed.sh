1ea2b478f8f58ef61eb22988652344d5f0ad2f89
Jeremi Biernacki <jeremi360@gmail.com>
2017-04-29 19:24:35 +0200

    remove old themes and BlueDark

--- oomox-numix/oomox-ClearLookN/assets/sed.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sed -i \
-         -e 's/#EDECEB/rgb(0%,0%,0%)/g' \
-         -e 's/#000000/rgb(100%,100%,100%)/g' \
-    -e 's/#EDECEB/rgb(50%,0%,0%)/g' \
-     -e 's/#8db0dc/rgb(0%,50%,0%)/g' \
-     -e 's/#FFFFFF/rgb(50%,0%,50%)/g' \
-     -e 's/#1A1A1A/rgb(0%,0%,50%)/g' \
-	$@
